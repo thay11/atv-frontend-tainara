3ecc6cf55493570a0227abf1ef0c8beeb88e8037
Your Name <you@example.com>
2024-07-25 10:38:22 -0500

    atv pag. index e login lh games

---
+++ js/script.js
@@ -0,0 +1,37 @@
+//efeito do botão voltar ao Topo
+
+function topo(){
+    window.scrollTo({ 
+            top:0,
+            left:0,
+            behavior:'smooth'
+        
+        }
+    )
+}   
+   
+//Validação de Login
+
+function login(){ 
+    var logado = 0;
+    var usuario = document.getElementById("usuario").value
+    var senha = document.getElementById("senha").value
+
+    if(usuario == 'admin' && senha == '123456'){
+        window.location = "index.html";
+        logado = 1;
+    }
+   
+    if(logado == 0){
+        alert("acesso negado. dados incoretos");
+    }
+
+} 
+
+//Ativar alert no botão cadrasta
+
+function cadastro(){
+    alert("cadastro com sucesso!");
+    window.location.href -"index.html"
+}
+ 
